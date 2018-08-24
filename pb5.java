e9837c993f0e0f046b9660fb20ad6c65860ededd
chandu044 <41730209+chandu044@users.noreply.github.com>
2018-08-24 12:42:36 +0530

    Create pb5.java

---
+++ pb5.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int p=s.nextInt();
+		int t=s.nextInt();
+		int r=s.nextInt();
+		int si=((p*t*r)/100);
+		System.out.print(si);
+	}
+}
